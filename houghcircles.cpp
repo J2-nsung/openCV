db427cf5e0f3908e7c5084924dcdb9aa3fe16ff6
Jinsung <goflrkqk@naver.com>
2019-10-23 14:36:55 +0900

    openCV

---
+++ houghcircles.cpp
@@ -0,0 +1,40 @@
+// ConsoleApplication1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "opencv2/opencv.hpp"
+#include<iostream>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat src = imread("../_res/coins.png", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed" << endl;
+		return -1;
+	}
+
+	Mat blurred;
+	blur(src, blurred, Size(3, 3));
+	
+	vector<Vec3f> circles;
+	HoughCircles(blurred, circles, HOUGH_GRADIENT, 1, 50, 150, 30);
+
+	Mat dst;
+	cvtColor(src, dst, COLOR_GRAY2BGR);
+
+	for (Vec3f c : circles) {
+		Point center(cvRound(c[0]), cvRound(c[1]));
+		int radius = cvRound(c[2]);
+		circle(dst, center, radius, Scalar(0, 0, 255), 2, LINE_AA);
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+
+	waitKey();
+	destroyAllWindows();
+
+	return 0;
+}
